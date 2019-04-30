de0e2a3c3fd0f51b829ffd703ffd716096c3d625
Utako Ueda <uueda@pivotal.io>
2019-04-30 11:58:25 -0700

    add basic stuff

---
+++ scripts/get_istio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eu
+
+clone_if_not_exist() {
+  local remote=$1
+  local dst_dir="$2"
+  echo "Cloning $remote into $dst_dir"
+  if [[ ! -d $dst_dir ]]; then
+    git clone "$remote" "$dst_dir"
+  fi
+}
+
+istio_path="${GOPATH}/src/istio.io/istio"
+
+clone_if_not_exist http://github.com/istio/istio "${istio_path}"
+
+echo "Getting dependencies..."
+pushd "${istio_path}"
+  dep ensure
+popd
+echo "Done!"
---
+++ scripts/istio_docker.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+istio_docker() {
+  local istio_dir
+  istio_dir="${1}"
+
+  if [[ -z "${istio_dir}" ]]; then
+    echo "WARNING: istio_dir not set"
+    echo "Setting istio directory to ~/workspace/istio-playground/src/"
+    echo "You may optionally pass your preferred istio directory as the first argument 😀 "
+    istio_dir="${HOME}/workspace/istio-playground/src/"
+  else
+    echo "istio_directory set to ${istio_dir}"
+  fi
+
+  echo "Getting istio/ci tags..."
+  local tag
+  tag=$(curl -s "https://hub.docker.com/v2/repositories/istio/ci/tags/" | jq -r '.results|.[0].name')
+
+  echo "Getting most recent istio/ci images..."
+  docker pull istio/ci:"${tag}"
+
+  local image_id
+  image_id=$(docker images -f reference=istio/ci --format "{{.ID}}" | head -n1)
+
+  docker run -u root -it --cap-add=NET_ADMIN -v "${istio_dir}":/go/src/ "${image_id}" /bin/bash
+}
+
+istio_docker "$@"
