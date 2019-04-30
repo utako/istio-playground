23bc7ee7dd5d9e4674c4f66796f7be0717f165c9
Utako Ueda <uueda@pivotal.io>
2019-04-30 14:43:26 -0700

    add docker container setup script

---
+++ scripts/setup_docker_dependencies.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "Downloading and installing helm..."
+curl -L https://git.io/get_helm.sh | bash
+
+echo "Getting kubectl..."
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+
+echo "Making kubectl executable..."
+chmod +x ./kubectl
+
+echo "Moving kubectl binary to PATH..."
+mv ./kubectl /usr/local/bin/kubectl
+
+echo "DONE!"
+kubectl version
